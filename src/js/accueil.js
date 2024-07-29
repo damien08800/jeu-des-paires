e53504f684a740cc816c891591c133c79ccb0c0a
damien <damiengalat@gmail.com>
2024-07-29 03:17:55 +0200

    ajout selecteur du nombres de cartes

--- src/js/accueil.js
+++ src/js/accueil.js
@@ -1,50 +1,61 @@
 const Direction = {
-    left: 'left',
-    right: 'right',
-  };
+  left: 'left',
+  right: 'right',
+};
   
-  let items = document.querySelectorAll('.item');
-  let maxLength = items.length;
-  let slideWrapper = document.querySelector('.wrap');
-  
-  if (maxLength >= 3) {
-    let leftButton = '<svg class="custom-arrows custom-arrows-left" data-action="left" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>';
-    let rightButton = '<svg class="custom-arrows custom-arrows-right" data-action="right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>';
-  
-    slideWrapper.insertAdjacentHTML('afterbegin', leftButton);
-    slideWrapper.insertAdjacentHTML('beforeend', rightButton);
+let items = document.querySelectorAll('.item');
+let maxLength = items.length;
+let slideWrapper = document.querySelector('.wrap');
 
-    antispam = true;
-  
-    document.addEventListener('click', (evt) => {
-      let eventTarget = evt.target;
-      if (eventTarget.classList.contains('custom-arrows') && antispam) {
+if (maxLength >= 3) {
+  let leftButton = '<svg class="custom-arrows custom-arrows-left" data-action="left" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>';
+  let rightButton = '<svg class="custom-arrows custom-arrows-right" data-action="right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>';
+
+  slideWrapper.insertAdjacentHTML('afterbegin', leftButton);
+  slideWrapper.insertAdjacentHTML('beforeend', rightButton);
+
+  antispam = true;
+
+  document.addEventListener('click', (evt) => {
+    let eventTarget = evt.target;
+    if (eventTarget.classList.contains('custom-arrows') && antispam) {
+      antispam = !antispam;
+      nextSlide(eventTarget.getAttribute('data-action'));
+      setTimeout(() => {
         antispam = !antispam;
-        nextSlide(eventTarget.getAttribute('data-action'));
-        setTimeout(() => {
-          antispam = !antispam;
-        }, "900");
-      }
+      }, "900");
+    }
+  });
+}
+
+items.forEach((it, index) => {
+  it.setAttribute('position', index + 1);
+});
+
+function nextSlide(direction) {
+  if (Direction.right === direction) {
+    items.forEach((it, index) => {
+      let prevValue = +it.getAttribute('position');
+      let nextValue = prevValue - 1 < 1 ? maxLength : prevValue - 1;
+      it.setAttribute('position', nextValue);
+    });
+  } else {
+    items.forEach((it, index) => {
+      let prevValue = +it.getAttribute('position');
+      let nextValue = prevValue + 1 > maxLength ? 1 : prevValue + 1;
+      it.setAttribute('position', nextValue);
     });
   }
-  
-  items.forEach((it, index) => {
-    it.setAttribute('position', index + 1);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const buttons = document.querySelectorAll('#many_card_selector div button');
+
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      buttons.forEach(btn => btn.classList.remove('active'));
+
+      button.classList.add('active');
+    });
   });
-  
-  function nextSlide(direction) {
-    if (Direction.right === direction) {
-      items.forEach((it, index) => {
-        let prevValue = +it.getAttribute('position');
-        let nextValue = prevValue - 1 < 1 ? maxLength : prevValue - 1;
-        it.setAttribute('position', nextValue);
-      });
-    } else {
-      items.forEach((it, index) => {
-        let prevValue = +it.getAttribute('position');
-        let nextValue = prevValue + 1 > maxLength ? 1 : prevValue + 1;
-        it.setAttribute('position', nextValue);
-      });
-    }
-  }
-  
+});
