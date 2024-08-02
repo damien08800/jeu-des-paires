745b83b42bf5c7211e9669300e699275dcdb1aeb
damien <damiengalat@gmail.com>
2024-08-02 18:38:01 +0200

    finition grille css pour les cartes selon leur nombres

--- game/game.js
+++ game/game.js
@@ -11,14 +11,14 @@ document.addEventListener('DOMContentLoaded', function() {
         link.type = 'text/css';
 
         switch (savedManyCards) {
-            case 8:
-                link.href = `css/8.css`;
+            case 6:
+                link.href = `css/6.css`;
                 break;
-            case 16:
-                link.href = `css/16.css`;
+            case 10:
+                link.href = `css/10.css`;
                 break;
-            case 32:
-                link.href = `css/32.css`;
+            case 14:
+                link.href = `css/14.css`;
                 break;
             default:
                 console.error('Nombre de cartes non supporté');
@@ -29,10 +29,26 @@ document.addEventListener('DOMContentLoaded', function() {
 
         for (let i = 0; i < savedManyCards; i++) {
             const divCard = document.createElement('div');
-            divCard.classList.add('div-card', `card-${i+1}`);
+
+            if(i+1 <= savedManyCards/2){
+                divCard.classList.add('div-card', `card-${i+1}`, 'first-line');
+            }
+            else{
+                divCard.classList.add('div-card', `card-${i+1}`, 'last-line');
+            }
+
+            const backImage = document.createElement('span')
+            const frontImage = document.createElement('span')
+            backImage.classList.add('back-image')
+            frontImage.classList.add('front-image')
+            backImage.src = `../src/img/onepiece/onepieceback.png`;
+
+            divCard.appendChild(backImage);
+            divCard.appendChild(frontImage);
             gameDiv.appendChild(divCard);
         }
     } else {
         console.error('Aucune valeur trouvée pour le nombre de cartes');
     }
 });
+
