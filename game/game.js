36ba026a20c905dede850b00eaebc000d6eddb63
damien <damiengalat@gmail.com>
2024-08-05 21:52:44 +0200

    ajout des images sur le thème naruto

--- game/game.js
+++ game/game.js
@@ -1,19 +1,48 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const savedManyCards = parseInt(localStorage.getItem('selectedManyCards'));
-    const savedSkin = localStorage.getItem('selectedSkin');
+    const SAVED_MANY_CARDS = parseInt(localStorage.getItem('selectedManyCards'));
+    const SAVED_SKIN = localStorage.getItem('selectedSkin');
+    const MUTED = localStorage.getItem('muted');
 
-    console.log(savedManyCards, savedSkin);
+    console.log(SAVED_MANY_CARDS, SAVED_SKIN, MUTED);
 
-    //##################################################################
-    //### chargement du bon fichier css
-    //################################################################## 
-    const gameDiv = document.getElementById("game");
-    if (savedManyCards) {
+    function shuffle(arr) {
+        const arrCopy = [...arr];
+        for (let i = arrCopy.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [arrCopy[i], arrCopy[j]] = [arrCopy[j], arrCopy[i]];
+        }
+        return arrCopy;
+    }
+    
+    function randomCardsList(){
+        let tab = [];
+        for (let i = 1; i <= 7; i++){
+            tab.push(i);
+        }
+        const shuffledNewTab = shuffle(shuffle(tab).slice(0, SAVED_MANY_CARDS/2));
+
+        // Doublage des valeurs dans le tableau
+        let doubledTab = [];
+        for (let value of shuffledNewTab) {
+            doubledTab.push(value, value);
+        }
+
+        return shuffle(doubledTab);
+    }
+
+    if (SAVED_MANY_CARDS && SAVED_SKIN) {
+
+        const GAME_DIV = document.getElementById("game");
+
+
+        //##################################################################
+        //### chargement du bon fichier css
+        //################################################################## 
         const link = document.createElement('link');
         link.rel = 'stylesheet';
         link.type = 'text/css';
 
-        switch (savedManyCards) {
+        switch (SAVED_MANY_CARDS) {
             case 6:
                 link.href = `css/6.css`;
                 break;
@@ -31,78 +60,96 @@ document.addEventListener('DOMContentLoaded', function() {
         
         
         //##################################################################
-        //### ajout des div et de la fonction flip
+        //### ajout des div et des images en background de la fonction flip
         //################################################################## 
+
+        const loadRandomCards = () =>{
+            let cardsList = randomCardsList();
+
+            for (let i = 0; i < SAVED_MANY_CARDS; i++) {
+                var divCard = document.createElement('div');
+                if(i+1 <= SAVED_MANY_CARDS/2){
+                    divCard.classList.add('div-card', `card-${i+1}`, 'first-line');
+                }
+                else{
+                    divCard.classList.add('div-card', `card-${i+1}`, 'last-line');
+                }
+    
+                var container = document.createElement('div');
+                container.classList.add('cards-container');
+    
+                var backImage = document.createElement('span');
+                backImage.classList.add('back-image');
+                
+                var frontImage = document.createElement('span');
+                frontImage.classList.add('front-image');
+
+                switch (SAVED_SKIN) {
+                    case "onepiece":
+                        backImage.style.backgroundImage = "url('../src/img/onepiece/onepieceback.png')";
+                        frontImage.style.backgroundImage = `url('../src/img/onepiece/${cardsList[i]}.png')`;
+                        break;
+                    case "naruto":
+                        backImage.style.backgroundImage = "url('../src/img/naruto/narutoback.png')";
+                        frontImage.style.backgroundImage = `url('../src/img/naruto/${cardsList[i]}.png')`;
+                        break;
+                    case "jujutsu kaisen":
+                        backImage.style.backgroundImage = "url('../src/img/onepiece/onepieceback.png')";
+                        frontImage.style.backgroundImage = `url('../src/img/onepiece/${cardsList[i]}.png')`;
+                        break;
+                    case "hunter x hunter":
+                        backImage.style.backgroundImage = "url('../src/img/onepiece/onepieceback.png')";
+                        frontImage.style.backgroundImage = `url('../src/img/onepiece/${cardsList[i]}.png')`;
+                        break;
+                    default:
+                        backImage.style.backgroundImage = "url('../src/img/onepiece/onepieceback.png')";
+                        frontImage.style.backgroundImage = `url('../src/img/onepiece/${cardsList[i]}.png')`;
+                        return; 
+                }
+                
+                container.addEventListener('click', function(){
+                    flipCard(this);
+                })
+                
+                container.appendChild(backImage);
+                container.appendChild(frontImage);
+                divCard.appendChild(container); 
+                GAME_DIV.appendChild(divCard);
+            }
+        }
+        loadRandomCards();
+        console.log(randomCardsList());
+
+
+
+
         const flipCard = (container) =>{
             container.classList.toggle('flip');
         }
 
-        for (let i = 0; i < savedManyCards; i++) {
-            const divCard = document.createElement('div');
-            if(i+1 <= savedManyCards/2){
-                divCard.classList.add('div-card', `card-${i+1}`, 'first-line');
-            }
-            else{
-                divCard.classList.add('div-card', `card-${i+1}`, 'last-line');
-            }
 
-            const container = document.createElement('div');
-            container.classList.add('cards-container');
+        //##################################################################
+        //### mise en place du titre selon le thème
+        //################################################################## 
+        let title = document.querySelector('#theme_name h3');
+        title.textContent = SAVED_SKIN;
 
-            const backImage = document.createElement('span')
-            const frontImage = document.createElement('span')
-            backImage.classList.add('back-image')
-            frontImage.classList.add('front-image')
+        //##################################################################
+        //### fonctionnement de la navigation (leave et reset)
+        //##################################################################
+        const leaveButton = document.querySelector('#leave');
+        const resetButton = document.querySelector('#reset');
+        const timerButton = document.querySelector('#timer');
 
-            container.addEventListener('click', function(){
-                console.log('oui')
-                flipCard(container);
-            })
+        leaveButton.addEventListener("click", function(){
+            window.location.href = "../";
+        })
 
-            container.appendChild(backImage);
-            container.appendChild(frontImage);
-            divCard.appendChild(container); 
-            gameDiv.appendChild(divCard);
-        }
+
+
     } else {
-        console.error('Aucune valeur trouvée pour le nombre de cartes');
+        console.error('Aucune valeur trouvée pour le nombre de cartes ou de thème');
     }
-
-    //##################################################################
-    //### mise en place du titre selon le thème
-    //################################################################## 
-    let title = document.querySelector('#theme_name h3');
-    title.textContent = savedSkin;
-
-    //##################################################################
-    //### fonctionnement de la navigation (leave et reset)
-    //##################################################################
-    const leaveButton = document.querySelector('#leave');
-    const resetButton = document.querySelector('#reset');
-    const timerButton = document.querySelector('#timer');
-
-    leaveButton.addEventListener("click", function(){
-        window.location.href = "../";
-    })
-
-
-    function shuffle(arr) {
-        const arrCopy = [...arr];
-        for (let i = arrCopy.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [arrCopy[i], arrCopy[j]] = [arrCopy[j], arrCopy[i]];
-        }
-        return arrCopy;
-    }
-    
-    function randomCards(){
-        let tab = [];
-        for (let i = 1; i <= parseInt(savedManyCards); i++){
-            tab.push(i);
-        }
-        return shuffle(tab);
-    }
-    console.log(randomCards());
 
 });
 
